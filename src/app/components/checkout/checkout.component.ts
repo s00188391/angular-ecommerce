0fb16f4034c2c1f5ad63496314fff428f4d5bd9d
S00188508 <S00188508@mail.itsligo.ie>
2023-05-09 13:18:55 +0100

    Revert "added dropdown lists for checkout form"
    This reverts commit 0b61d27d7ca1f8c97eaa1b9fb5559fc5e382d5a1.

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { State } from 'src/app/common/state';
-import { start } from 'repl';
-import { Country } from 'src/app/common/country';
-import { EcommerceFormService } from 'src/app/services/ecommerce-form.service';
 
 @Component({
   selector: 'app-checkout',
@@ -18,16 +14,7 @@ export class CheckoutComponent implements OnInit {
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
-  creditCardYears: number[] = [];
-  creditCardMonths: number[] = [];
-
-  countries: Country[] = [];
-
-  shippingAddressStates: State[] =[];
-  billingAddressStates: State[] =[];
-
-  constructor(private formBuilder: FormBuilder,
-              private ecommerceFormService: EcommerceFormService ) { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
@@ -59,33 +46,6 @@ export class CheckoutComponent implements OnInit {
         expirationYear: ['']
       })
     });
-
-    // populate credit card months
-    const startMonth: number = new Date().getMonth() + 1;
-    console.log("startMonth: " + startMonth);
-
-    this.ecommerceFormService.getCreditCardMonths(startMonth).subscribe(
-      data => {
-        console.log("Retrieved credit card months: " + JSON.stringify(data));
-        this.creditCardMonths = data;
-      }
-    );
-
-    // populate credit card years
-    this.ecommerceFormService.getCreditCardYears().subscribe(
-      data => {
-        console.log("Retrieved credit card years: " + JSON.stringify(data));
-        this.creditCardYears = data;
-      }
-    );
-
-    //populate countries
-    this.ecommerceFormService.getCountries().subscribe(
-      data => {
-        console.log("Retrieved countries: " + JSON.stringify(data));
-        this.countries = data;
-      }
-    );
   }
 
   copyShippingAddressToBillingAddress(event) {
@@ -102,55 +62,5 @@ export class CheckoutComponent implements OnInit {
     console.log("Handling submit button")
     console.log(this.checkoutFormGroup.get('customer').value);
     console.log("The email address is: " + this.checkoutFormGroup.get('customer').value.email);
-  }
-
-  handleMonthsAndYears() {
-
-    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
-
-    const currentYear: number = new Date().getFullYear();
-    const selectedYear: number = Number(creditCardFormGroup.value.expirationYear);
-
-    // if the current year equals the selected year, then start with the current month
-
-    let startMonth: number;
-
-    if (currentYear === selectedYear) {
-      startMonth = new Date().getMonth() + 1;
-    }
-    else {
-      startMonth = 1;
-    }
-
-    this.ecommerceFormService.getCreditCardMonths(startMonth).subscribe(
-      data => {
-        console.log("Retrieved credit card months: " + JSON.stringify(data));
-        this.creditCardMonths = data;
-      }
-    );
-  }
-  getStates(formGroupName: string) {
-
-    const formGroup = this.checkoutFormGroup.get(formGroupName);
-
-    const countryCode = formGroup.value.country.code;
-    const countryName = formGroup.value.country.name;
-
-    console.log(`{formGroupName} country code: ${countryCode}`);
-    console.log(`{formGroupName} country name: ${countryName}`);
-
-    this.ecommerceFormService.getStates(countryCode).subscribe(
-      data => {
-        if (formGroupName === 'shippingAdress') {
-          this.shippingAddressStates = data;
-        }
-        else {
-          this.billingAddressStates = data;
-        }
-
-        // select first item by default
-        formGroup.get('state').setValue(data[0]);
-      }
-    );
   }
 }
